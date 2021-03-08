31830541aa69eda774fc2040100cbcfb7965d53b
Jim Nguyen <jimnguyen220@gmail.com>
2021-03-07 23:53:42 -0600

    combined inquirer into showDept and started createEmployee

--- server.js
+++ server.js
@@ -40,21 +40,11 @@ function startInquirer () {
         if (data.prompt === "Display All Employees") {
             showTeam();
         } else if (data.prompt === "Display Employees by Department") {
-            inquirer.prompt([
-                {
-                    type: "list",
-                    name: "department",
-                    message: "What department would you like to see?",
-                    choices: ["Sales", "Engineering", "Finance", "Legal"]
-                }
-            ]).then((response) => {
-                let department = response.department;
-                showDepartment(department);
-            })
-
+            showDepartment();
         } else if (data.prompt === "Display Employees by Manager") {
 
         } else if (data.prompt === "Add an Employee") {
+            createEmployee();
 
         } else if (data.prompt === "Update an Employee") {
         
@@ -80,17 +70,80 @@ function showTeam () {
     })
 };
 
-function showDepartment (department) {
-    let sql = 
-    `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee INNER JOIN role ON employee.role_id = role.role_id INNER JOIN department ON role.department_id = department.department_id WHERE department.department_name = "${department}"`;
+function showDepartment () {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "department",
+            message: "What department would you like to see?",
+            choices: ["Sales", "Engineering", "Finance", "Legal"]
+        }
+        ]).then((response) => {
+            const department = response.department;
+            let sql = 
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee INNER JOIN role ON employee.role_id = role.role_id INNER JOIN department ON role.department_id = department.department_id WHERE department_name = "${department}"`;
 
-    connection.query(sql, function (err, res){
-        if (err) throw err;
-        // console.log(res);
-        console.table(`${department} Team Employees`, res);
-        startInquirer();
+            connection.query(sql, function (err, res){
+                if (err) throw err;
+                // console.log(res);
+                console.table(`${department} Team Employees`, res);
+                startInquirer();
+        })
     })
 };
-
 ////////////////////////////////////////////////////////
 
+function createEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "last",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's id number?"
+        },
+        {
+            type: "list",
+            name: "department",
+            message: "Which department will the employee work in",
+            choices: ["Sales", "Engineering", "Finance", "Legal"]
+        },
+    ]).then((data) => {
+        console.log(data);
+
+        //The code below does not work
+        var query = connection.query(
+            "INSERT INTO employee SET ?",
+            {   
+                id: `"${data.id}"`,
+                first_name: `"${data.first}"`,
+                last_name: `"${data.last}"`,
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows);
+                // updateEmployee();
+            }
+        );
+    })
+
+}
+
+// function updateEmployee() {
+//     var query = connection.query(
+//         "UPDATE employee SET ? WHERE ?",
+//         [
+//             {
+
+//             }
+//         ]
+//     )
+// };
